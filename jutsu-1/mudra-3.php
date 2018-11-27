27625490eee83d4f36fa682a759c0a7d4c11e8be
KmeCnin <pierrechanel.gauthier@gmail.com>
2018-11-27 23:23:30 +0100

    Fix return

--- jutsu-1/mudra-3.php
+++ jutsu-1/mudra-3.php
@@ -25,44 +25,44 @@ function combineChakra($chakra1, $chakra2, $chakra3 = null){
         ($chakra1 == "fûton" || $chakra1 == "suiton") &&
         ($chakra2 == "suiton" || $chakra2 == "fûton")
     ){
-        echo "Hyôton"; // Glace
+        return "Hyôton"; // Glace
     } elseif (
         ($chakra1 == "doton" || $chakra1 == "suiton") &&
         ($chakra2 == "suiton" || $chakra2 == "doton")
     ){
-        echo "Mokuton"; // Bois
+        return "Mokuton"; // Bois
     } elseif (
         ($chakra1 == "raiton" || $chakra1 == "suiton") &&
         ($chakra2 == "suiton" || $chakra2 == "raiton")
     ){
-        echo "Ranton"; // Orage
+        return "Ranton"; // Orage
     } elseif (
         ($chakra1 == "katon" || $chakra1 == "doton") &&
         ($chakra2 == "doton" || $chakra2 == "katon")
     ){
-        echo "Yôton"; // Lave
+        return "Yôton"; // Lave
     } elseif (
         ($chakra1 == "katon" || $chakra1 == "suiton") &&
         ($chakra2 == "suiton" || $chakra2 == "katon")
     ){
-        echo "Futton"; // Vapeur
+        return "Futton"; // Vapeur
     } elseif (
         ($chakra1 == "doton" || $chakra1 == "raiton") &&
         ($chakra2 == "raiton" || $chakra2 == "doton")
     ){
-        echo "Bakuton"; // Explosion
+        return "Bakuton"; // Explosion
     } elseif (
         ($chakra1 == "fûton" || $chakra1 == "katon") &&
         ($chakra2 == "katon" || $chakra2 == "fûton")
     ){
-        echo "Shakuton"; // Châleur
+        return "Shakuton"; // Châleur
     } elseif (
         ($chakra1 == "fûton" || $chakra1 == "doton") &&
         ($chakra2 == "doton" || $chakra2 == "fûton")
     ){
-        echo "Jiton"; // Magnétisme
+        return "Jiton"; // Magnétisme
     } else {
-        echo "Unknown";
+        return "Unknown";
     }
 }
 
