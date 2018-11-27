c5cb9a034bcd565e06fc203670c768c806efee24
KmeCnin <pierrechanel.gauthier@gmail.com>
2018-11-27 21:48:20 +0100

    Create jutsu 2

---
+++ jutsu-2/seal-1.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * Lighten function combineChakra by removing some ||.
+ * Store the parameter chakra into an array and then search into that array if the wanted values are in it.
+ *
+ * See:
+ * - http://php.net/manual/en/function.array.php
+ */
+
+$doton = "doton";
+$suiton = "suiton";
+$katon = "katon";
+$raiton = "raiton";
+$futon = "fûton";
+
+function combineChakra($chakra1, $chakra2, $chakra3 = null){
+    $chakras = [$chakra1, $chakra2, $chakra3];
+    if (in_array("doton", $chakras) && in_array("fûton", $chakras) && in_array("katon", $chakras)){
+        return "Jinton";
+    } elseif (in_array("fûton", $chakras) && in_array("suiton", $chakras)){
+        return "Hyôton";
+    } elseif (in_array("doton", $chakras) && in_array("suiton", $chakras)){
+        return "Mokuton";
+    } elseif (in_array("raiton", $chakras) && in_array("suiton", $chakras)){
+        return "Ranton";
+    } elseif (in_array("katon", $chakras) && in_array("doton", $chakras)){
+        return "Hyôton";
+    } elseif (in_array("katon", $chakras) && in_array("suiton", $chakras)){
+        return "Futton";
+    } elseif (in_array("doton", $chakras) && in_array("raiton", $chakras)){
+        return "Bakuton";
+    } elseif (in_array("fûton", $chakras) && in_array("katon", $chakras)){
+        return "Shakuton";
+    } elseif (in_array("fûton", $chakras) && in_array("doton", $chakras)){
+        return "Jiton";
+    } elseif (in_array("fûton", $chakras) && in_array("suiton", $chakras)){
+        return "Hyôton";
+    } else {
+        return "Unknown";
+    }
+}
+
+$combinedChakra = combineChakra($doton, $futon, $katon);
+echo $combinedChakra;
---
+++ jutsu-2/seal-2.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * Directly allow the function combineChakra to receive any number of chakras.
+ *
+ * See:
+ * - http://php.net/manual/en/function.array.php
+ */
+
+$doton = "doton";
+$suiton = "suiton";
+$katon = "katon";
+$raiton = "raiton";
+$futon = "fûton";
+
+function combineChakra($chakras){
+    if (in_array("doton", $chakras) && in_array("fûton", $chakras) && in_array("katon", $chakras)){
+        return "Jinton";
+    } elseif (in_array("fûton", $chakras) && in_array("suiton", $chakras)){
+        return "Hyôton";
+    } elseif (in_array("doton", $chakras) && in_array("suiton", $chakras)){
+        return "Mokuton";
+    } elseif (in_array("raiton", $chakras) && in_array("suiton", $chakras)){
+        return "Ranton";
+    } elseif (in_array("katon", $chakras) && in_array("doton", $chakras)){
+        return "Hyôton";
+    } elseif (in_array("katon", $chakras) && in_array("suiton", $chakras)){
+        return "Futton";
+    } elseif (in_array("doton", $chakras) && in_array("raiton", $chakras)){
+        return "Bakuton";
+    } elseif (in_array("fûton", $chakras) && in_array("katon", $chakras)){
+        return "Shakuton";
+    } elseif (in_array("fûton", $chakras) && in_array("doton", $chakras)){
+        return "Jiton";
+    } elseif (in_array("fûton", $chakras) && in_array("suiton", $chakras)){
+        return "Hyôton";
+    } else {
+        return "Unknown";
+    }
+}
+
+$combinedChakra = combineChakra([$doton, $futon, $katon]);
+echo $combinedChakra;
